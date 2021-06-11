fc9b2534c235aad2b71c514844aec073aebfaf01
jehux <jehunieves@gmail.com>
2021-06-11 09:15:47 -0500

    Fix: catalogo

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const formulario = document.querySelector('#formulario')
 const email = document.querySelector('#email')
 const pass = document.querySelector('#pass')
 const error = document.querySelector('#error')
+const nombreUser = document.querySelector('#nombreUser')
+const loginBoton = document.querySelector('#login-btn')
 
 
 // Initialize Firebase
@@ -46,52 +48,74 @@ var db = firebase.firestore();
 
   var libros = document.querySelector('#libro')
 
-
-var docRef = db.collection("Libro")
-docRef.get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        //console.log(`${doc.id} => ${doc.data()}`);
-        //console.log(doc.data().Titulo);
-        libros.innerHTML += `
-        <div class="col-md-4 mb-5">
-            <div class="info">
-              <div class="icon icon-lg icon-shape icon-shape-primary shadow rounded-circle">
-                <i class="ni ni-book-bookmark"></i>
+if (libros != null){
+  var docRef = db.collection("Libro")
+  docRef.get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+          //console.log(`${doc.id} => ${doc.data()}`);
+          //console.log(doc.data().Titulo);
+          libros.innerHTML += `
+          <div class="col-md-4 mb-5">
+              <div class="info">
+                <div class="icon icon-lg icon-shape icon-shape-primary shadow rounded-circle">
+                  <i class="ni ni-book-bookmark"></i>
+                </div>
+                <h6 class="info-title text-uppercase text-primary">${doc.data().Titulo}</h6>
+                <p class="description opacity-8">${doc.data().Notas}</p>
+                <a href="javascript:;" class="text-primary">Ver libro
+                  <i class="ni ni-bold-right text-primary"></i>
+                </a>
               </div>
-              <h6 class="info-title text-uppercase text-primary">${doc.data().Titulo}</h6>
-              <p class="description opacity-8">${doc.data().Notas}</p>
-              <a href="javascript:;" class="text-primary">Ver libro
-                <i class="ni ni-bold-right text-primary"></i>
-              </a>
             </div>
-          </div>
-        `
-    });
-});
+          `
+      });
+  });
+}
 
-
-formulario.addEventListener('submit', (e) => {
-  e.preventDefault()
-  if(!email.value.trim()){
-      console.log('input vacio')
-      return
-  }else if(!pass.value.trim()){
-      console.log('input vacio')
-      return
-  }
-  
+if ( nombreUser !=null ){
+  console.log(document.cookie)
+  var cookieValor = document.cookie.replace(/(?:(?:^|.*;\s*)idUsuario\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+  var nombre = ""
   firebase.firestore().collection('Usuario')
-      .onSnapshot(query => {
-          query.forEach(doc =>{
-              console.log(doc.data())
-              if(doc.data().Correo == email.value && doc.data().Password == pass.value){
-                  console.log('Encontrado')
-              }
-              else{
-                  console.log('No encontrado')
-                  return
-              }
-          })
+  .onSnapshot(query => {
+      query.forEach(doc =>{
+          if(doc.id === cookieValor ){
+              nombre = doc.data().Nombre +" "+doc.data().Apellido
+              nombreUser.innerHTML = `<span class="nav-link-inner--text">${nombre}</span>`
+              loginBoton.innerHTML = ""
+          }
       })
+  })
+        
   
-})
+
+}
+if (formulario != null){
+  formulario.addEventListener('submit', (e) => {
+    e.preventDefault()
+    if(!email.value.trim()){
+        console.log('input vacio')
+        return
+    }else if(!pass.value.trim()){
+        console.log('input vacio')
+        return
+    }
+    
+    firebase.firestore().collection('Usuario')
+        .onSnapshot(query => {
+            query.forEach(doc =>{
+                //console.log(doc.data())
+                if(doc.data().Correo == email.value && doc.data().Password == pass.value){
+                    console.log('Encontrado',doc.id)
+                    document.cookie = "idUsuario="+doc.id;
+                    window.location="index.html";
+                }
+                else{
+                    console.log('No encontrado')
+                    return
+                }
+            })
+        })
+    console.log(document.cookie)
+  })
+}
