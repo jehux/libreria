cdfbd26b1f6c3b01482863f84f562089a532512c
jehux <jehunieves@gmail.com>
2021-06-10 23:02:32 -0500

    Fix: index y agregar catalogo

--- app.js
+++ app.js
@@ -45,23 +45,18 @@ docRef.get().then((querySnapshot) => {
         //console.log(`${doc.id} => ${doc.data()}`);
         console.log(doc.data().Titulo);
         libros.innerHTML += `
-        </div>
-        <div class="card" style="width: 18rem;">
-        <img src="https://4.bp.blogspot.com/-p3WLzI73jiU/W3F9nOfRZdI/AAAAAAAABOU/6Umu0QlXe4UZCnvE_MR2_8tJh2NWZ1jPwCLcBGAs/s1600/libro.png" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">${doc.data().Titulo}</h5>
-            <p class="card-text">${doc.data().Notas}</p>
-        </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">Autor: ${doc.data().Autor}</li>
-            <li class="list-group-item">Edicion: ${doc.data().Edicion}</li>
-            <li class="list-group-item">Editorial: ${doc.data().Editorial}</li>
-        </ul>
-        <div class="card-body">
-            <a href="#" class="card-link">Ver libro</a>
-            <a href="#" class="card-link">Seleccionar cita</a>
-        </div>
-        </div>
+        <div class="col-md-4">
+            <div class="info">
+              <div class="icon icon-lg icon-shape icon-shape-primary shadow rounded-circle">
+                <i class="ni ni-book-bookmark"></i>
+              </div>
+              <h6 class="info-title text-uppercase text-primary">${doc.data().Titulo}</h6>
+              <p class="description opacity-8">${doc.data().Notas}</p>
+              <a href="javascript:;" class="text-primary">Ver libro
+                <i class="ni ni-bold-right text-primary"></i>
+              </a>
+            </div>
+          </div>
         `
     });
 });
