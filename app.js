e3911ec668399d3456e1206fcee50f71800d2c98
jehux <jehunieves@gmail.com>
2021-06-10 22:39:58 -0500

    add: lista libros

--- app.js
+++ app.js
@@ -35,3 +35,33 @@ db.collection("Libro")
       console.log(`${doc.id} => ${doc.data()}`);
     });
   });
+
+  var libros = document.querySelector('#libro')
+
+
+var docRef = db.collection("Libro")
+docRef.get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        //console.log(`${doc.id} => ${doc.data()}`);
+        console.log(doc.data().Titulo);
+        libros.innerHTML += `
+        </div>
+        <div class="card" style="width: 18rem;">
+        <img src="https://4.bp.blogspot.com/-p3WLzI73jiU/W3F9nOfRZdI/AAAAAAAABOU/6Umu0QlXe4UZCnvE_MR2_8tJh2NWZ1jPwCLcBGAs/s1600/libro.png" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${doc.data().Titulo}</h5>
+            <p class="card-text">${doc.data().Notas}</p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Autor: ${doc.data().Autor}</li>
+            <li class="list-group-item">Edicion: ${doc.data().Edicion}</li>
+            <li class="list-group-item">Editorial: ${doc.data().Editorial}</li>
+        </ul>
+        <div class="card-body">
+            <a href="#" class="card-link">Ver libro</a>
+            <a href="#" class="card-link">Seleccionar cita</a>
+        </div>
+        </div>
+        `
+    });
+});
